eecd36c691429cdef9380ac512615cb4a1fd8ee1
Kushagra Varade <kushagra@robustest.com>
2023-02-14 14:28:07 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const wdio = require("webdriverio");
+
+// Chromedriver to be used is 26.03, Ass mentioned in the link: https://github.com/headspinio/appium-lg-webos-driver#:~:text=remote%20Chromium%20debugging.-,appium%3AchromedriverExecutable,-%5BOptional%5D%20Most%20LG
+const opts = {
+    // path: '/wd/hub',
+    port: 4723,
+    capabilities: {
+        "platformName": "LGTV",
+        "appium:automationName" : "webOS",
+        "appium:deviceName" : "newTv",
+        "appium:chromedriverExecutable" : "/Users/kushagravarade/Downloads/chromedriver", // Path to your chromedriver
+        "appium:appPackage" : "com.robustets.app",
+        "appium:rcMode" : "js",
+    }
+  };
+
+let client 
+
+async function remoteAction(){
+  client = await wdio.remote(opts)
+  await client.executeScript('webos: pressKey', [{key: 'right', duration: 200}])
+}
+
+console.log("Chalega")
+remoteAction()
